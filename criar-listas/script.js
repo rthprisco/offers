43f0f02204e233c36935a5cf100387f9ae50ca66
laisaAraujo0 <araujolaisa515@gmail.com>
2024-06-23 16:47:10 -0300

    atual

--- criar-listas/script.js
+++ criar-listas/script.js
@@ -1,1 +1,19 @@
-import './style.css'
+const minus = document.querySelector(".minus")
+const max = document.querySelector(".max")
+const n = document.querySelector(".n")
+
+minus.addEventListener("click", function(){
+    if(n.value>1){
+        n.value -= 1
+    }
+    total()
+})
+max.addEventListener("click", function(){
+    (n.value) ++ 
+    total()
+})
+
+const valor = document.querySelector(".valor")
+function total(){
+    valor.innerHTML =120*  n.value
+}
