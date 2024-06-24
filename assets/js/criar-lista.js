e701c702818c139cccc0e617728c3d64abe7df2f
Arthur Prisco <arthurprisco45@gmail.com>
2024-06-24 16:15:19 -0300

    Pronto

---
+++ assets/js/criar-lista.js
@@ -0,0 +1,35 @@
+const input = document.getElementById("put");
+const lista = document.getElementById("lista-container");
+
+function AddTask() {
+    if (input.value === '') {
+        alert("Campo em branco");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = input.value;
+        lista.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    input.value = "";
+    saveData();
+}
+
+lista.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData()
+
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData()
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", lista.innerHTML);
+}
+function showTask() {
+    lista.innerHTML = localStorage.getItem("data");
+}
